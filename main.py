9a1f4a1bba4be26ec6c3e4dab5c140445edfdb62
satters94 <tom.satters@gmail.com>
2025-06-27 12:39:40 -0700

    Added prompt input

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+import sys
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
-
 client = genai.Client(api_key=api_key)
-response = client.models.generate_content(
-    model="gemini-2.0-flash-001",
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-)
-print(f"Text response: {response.text}")
+inputs = sys.argv
 
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+def main():
+    if len(inputs) > 1: 
+        response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=inputs[1]
+        )
+        print(f"Text response: {response.text}")
+
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+    else:
+        error_message = "No prompt provided!"
+        print(error_message)
+        sys.exit(1)
+
+
+if __name__ == "__main__":
+    main()
